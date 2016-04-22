ac7c99caa241a12a73901c7431664266449fdac4
Arne Cools <arnecools@hotmail.com>
2016-04-22 11:56:48 +0200

    multiplayer test

---
+++ multiplayer test/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class PlayerMove : NetworkBehaviour {
+	Rigidbody2D rb;
+	float jumpSpeed = 50f;
+	float velocity = 250f;
+	public Sprite localSprite;
+	public GameObject fireB;
+	SpriteRenderer sr;
+
+	float fireTimer = 1f;
+	float tempTime = 0f;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		
+	}
+
+	public override void OnStartLocalPlayer()
+	{
+		//sr.sprite = localSprite;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//if (Input.touchCount > 0)
+		//{
+		//    CmdFire();
+		//}
+
+		
+		tempTime += Time.deltaTime;
+
+		if (!isLocalPlayer)
+			return;
+		
+		if(Input.touchCount > 0 || Input.GetButton("Fire1"))
+		{
+			rb.AddForce(Vector2.up * jumpSpeed);
+            GameObject.Find("Counter").GetComponent<Text>().text = (int.Parse(GameObject.Find("Counter").GetComponent<Text>().text) + 1).ToString();
+		}
+
+	}
+
+	//[Command]
+	//void CmdFire()
+	//{ 
+		
+		
+	//        if(tempTime > fireTimer)
+	//        {
+	//            tempTime = 0f;
+
+	//            GameObject Fireball;
+	//            Fireball = Instantiate(fireB, transform.position, transform.rotation) as GameObject;
+	//            Fireball.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector3 (-velocity, 0));
+
+	//            NetworkServer.Spawn(Fireball);
+	//        }
+		
+	//}
+}
---
+++ multiplayer test/Assets/Scripts/fireBall.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fireBall : MonoBehaviour {
+    float velocity = 100f;
+    float timeToLive = 3f;
+    float tempTime = 0;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        tempTime += Time.deltaTime;
+        if(tempTime > timeToLive)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    void OnCollisonEnter(Collision2D collision)
+    {
+        GameObject hit = collision.gameObject;
+        bool hitPlayer = hit.GetComponent<PlayerMove>();
+        if(hitPlayer != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
