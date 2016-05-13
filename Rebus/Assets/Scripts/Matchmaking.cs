bc5cc04139a1b8d9d74747d94b17304fbab3872d
Sacha Meertens <sacha.meertens@hotmail.com>
2016-05-13 14:58:27 +0200

    App-master in orde brengen

---
+++ Rebus/Assets/Scripts/Matchmaking.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Matchmaking : MonoBehaviour {
+
+	NetworkManager nw;
+
+	// Use this for initialization
+	void Start () {
+		nw = GameObject.Find("NetworkManager").GetComponent<NetworkManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LANMatch()
+	{
+		nw.StartHost();
+	}
+
+	public void LANJoin()
+	{
+		nw.StartClient();
+	}
+
+	public void EnableOnline()
+	{
+		nw.StartMatchMaker();
+	}
+
+	void OnPlayerConnected ()
+	{
+		Debug.Log("Joined");
+	}
+}
---
+++ Rebus/Assets/Scripts/commandScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class commandScript : MonoBehaviour {
+
+    public bool sendAnswer = false;
+
+
+	public void validateAnswer()
+    {
+        GameObject.Find("localObject").GetComponent<Antwoord>().sendAnswer = true;
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
